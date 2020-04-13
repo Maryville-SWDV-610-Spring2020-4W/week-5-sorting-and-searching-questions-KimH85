08749dc277f5a137d255d378105d9a2834d35d13
KimH85 <61917958+KimH85@users.noreply.github.com>
2020-04-12 20:06:27 -0400

    Added more for the list methods

--- list_dictionary_example.py
+++ list_dictionary_example.py
@@ -1,13 +1,15 @@
 import math
 from random import randrange
 
-d_person_lucky_number = {}
-l_1thru10 = [1,2,3,4,5,6,7,8,9,10]
-    
+players_dictionary = {}
+lucky_number_list = []
+
 def main():
+    #creates a list of numbers that is used for drawing lucky numbers
+    setup_lucky_number_list()
     
     #creates the dictionary
-    setup_lucky_numbers() 
+    setup_players_dictionary() 
     
     #loop until winner declared
     have_winner = False
@@ -16,10 +18,25 @@ def main():
         #get a random number not already called and determine
         #if someone had it as a lucky number in dictionary
         have_winner = determine_winner(drawing_number())
-    
+
+#set up a list of numbers that will be drawn from
+def setup_lucky_number_list():
+    greatest_number = None
+    while greatest_number == None:
+        try:
+            greatest_number = int(input("Enter an integer that will be used to create the number pool of lucky numbers: "))
+            
+        except:
+            print("Must enter an integer value.  This value will be used to create the number pool to draw lucky numbers. Try again.")
+            
+    #create list with length set as the selected greatest number
+    #add sequential number into the list
+    for i in range(greatest_number):
+        lucky_number_list.append(i + 1)
+        
 
 #sets up dictionary with person and their lucky number    
-def setup_lucky_numbers():
+def setup_players_dictionary():
     
     #loop until name is not entered by user
     name = None
@@ -31,51 +48,54 @@ def setup_lucky_numbers():
             break
         
         #determine if name already entered in dictionary - error if true
-        if d_person_lucky_number.get(name) != None:
+        if players_dictionary.get(name) != None:
             print("This person is already entered.  One try per person please.")
         
         #if valid, continue...
         else:
             
             #adding entry into dictionary for key with no value.
-            d_person_lucky_number[name] = None
+            players_dictionary[name] = None
             
             #get person's lucky number and add to dictionary
             try:
-                lucky_number = int(input("Enter their lucky number between 1-10: "))
+                lucky_number = int(input("Enter their lucky number between 1-" + str(len(lucky_number_list)) + ":"))
                 
                 #validate the entry, handle error if invalid
-                if lucky_number < 1 or lucky_number > 10:
+                if lucky_number < 1 or lucky_number > len(lucky_number_list):
                     lucky_number_error(name)
                    
                 #otherwise update dictionary
                 else:
-                    d_person_lucky_number[name] = lucky_number
+                    players_dictionary[name] = lucky_number
                     
             except:
                 #if non-integer entered, handle error
                 lucky_number_error(name)
     
         
-    print("\nGood Luck: ", d_person_lucky_number)
+    print("\nGood Luck: ", players_dictionary)
     input("Press Enter to Continue...")
 
 #show error and remove person from dictionary
 def lucky_number_error(name):
-    print("Must enter an integer value between 1-10.  Try again.")
-    del d_person_lucky_number[name]
+    print("Must enter an integer value between 1-" + str(len(lucky_number_list)) + ".  Try again.")
+    del players_dictionary[name]
+    
     
 #draw a random number that has not already been selected
 def drawing_number():
     
     while True:
-        x = randrange(1,10)
+        x = randrange(1,len(lucky_number_list))
         
         #evaluate list index that should hold the value
         #and if value is still there - show and return
-        if l_1thru10[x-1] > 0:
+        if lucky_number_list[x-1] > 0:
             input("\nNumber is " + str(x) + ". Press Enter to Continue...")
             return x
+
+
 
 #using random number selected, determine if anyone selected
 #as their lucky number - if so announce winner.  If not,
@@ -85,8 +105,8 @@ def determine_winner(selected_number):
     is_winner = False
     
     #loop through dictionary to determine if anyone has selected number
-    for name in d_person_lucky_number:
-        if d_person_lucky_number[name] == selected_number:
+    for name in players_dictionary:
+        if players_dictionary[name] == selected_number:
             
             #winner declared - exit loop and return True
             print(name,"WON!  Congratulations!")
@@ -96,7 +116,7 @@ def determine_winner(selected_number):
     if is_winner == False:
         #if get here - no winner declared.  Update the
         #list, changing selected number to 0 in list and return False
-        l_1thru10[selected_number-1] = 0
+        lucky_number_list[selected_number-1] = 0
         print("No winner yet - getting next number.")
     
     return is_winner
